628af1d6b390ea995a3515ab17bdb0cfd19e6f42
nuret209 <noguc4200@gmail.com>
2025-01-28 01:44:52 +0300

    mobile

--- components/home/SearchAreaMenu.tsx
+++ components/home/SearchAreaMenu.tsx
@@ -14,11 +14,11 @@ const SearchAreaMenu = ({ search }: { search: string }) => {
               <div key={index} className='flex flex-col'>
                 {("submenus" in submenu) && <div className='px-3 py-2 text-label-xs mx-2 text-[#8f8f8f]'>{submenu.name}</div>}
                 {'submenus' in submenu ? submenu.submenus.map(subsubmenu =>
-                  (!search || subsubmenu.name.toLowerCase().includes(search.toLowerCase())) && <div key={subsubmenu.name} className='hovericon group text-[#c5c5c5]  flex items-center justify-between px-3 cursor-pointer py-2 mx-2 transition-all duration-200 lg:hover:bg-[#f7f7f7] text-gray-900 text-paragraph-md'>
+                  (!search || subsubmenu.name.toLowerCase().includes(search.toLowerCase())) && <div key={subsubmenu.name} className='hovericon group text-[#c5c5c5] lg:text-gray-700 flex items-center justify-between px-3 cursor-pointer py-2 mx-2 transition-all duration-200 lg:hover:bg-[#f7f7f7] text-gray-900 text-paragraph-md'>
                     {subsubmenu.name} <RiArrowRightSLine className='lg:opacity-0  text-[#525252] duration-200 transition-all group-hover:opacity-100' />
                   </div>) :
-                  (!search || submenu.name.toLowerCase().includes(search.toLowerCase())) && <div key={submenu.name} className='hovericon group text-[#c5c5c5]  flex items-center justify-between px-3 cursor-pointer py-2 mx-2 transition-all duration-200 lg:hover:bg-[#f7f7f7] text-gray-900 text-paragraph-md'>
-                    {submenu.name} <RiArrowRightSLine className='lg::opacity-0  text-[#525252] duration-200 transition-all group-hover:opacity-100' />
+                  (!search || submenu.name.toLowerCase().includes(search.toLowerCase())) && <div key={submenu.name} className='hovericon group text-[#c5c5c5] lg:text-gray-700  flex items-center justify-between px-3 cursor-pointer py-2 mx-2 transition-all duration-200 lg:hover:bg-[#f7f7f7] text-gray-900 text-paragraph-md'>
+                    {submenu.name} <RiArrowRightSLine className='lg:opacity-0  text-[#525252] duration-200 transition-all group-hover:opacity-100' />
                   </div>
                 }
                 {("submenus" in submenu) && <div className="px-5 lg:px-0  my-2">
