f1c1f8196438c8906f39201a44289903691dbcf8
nuret209 <noguc4200@gmail.com>
2025-02-12 14:24:01 +0300

    some

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const [menuActive, setMenuActive] = useState(false);
   return (
     <div className={` mx-auto w-full max-w-[1488px] flex-1 lg:grid lg:grid-cols-[280px_minmax(0,1fr)] lg:gap-14`}>
-      <Menu active={menuActive} setMenuActive={setMenuActive}/>
+      <Menu currentPage='/' active={menuActive} setMenuActive={setMenuActive}/>
      <div className={`${menuActive ? "overflow-hidden max-h-screen" : ""} `}><MainArea slug='/' setMenuActive={setMenuActive}/></div> 
     </div>
   );
