d01ea9b3d1e895426060dbdf9957a0a0fb7d8781
nuret209 <noguc4200@gmail.com>
2025-03-04 00:16:14 +0300

    some

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 "use client"
+import MainArea from '@/components/home/MainArea';
 import Menu from '@/components/home/Menu';
+import React from 'react';
 import { useState } from 'react';
-export default function Home() {
 
-  const [menuActive, setMenuActive] = useState(false);
-  return (
-    <div className={` mx-auto w-full max-w-[1488px] flex-1 lg:grid lg:grid-cols-[280px_minmax(0,1fr)] lg:gap-14`}>
-      <Menu active={menuActive} setMenuActive={setMenuActive} />
-      <div className={`${menuActive ? "overflow-hidden max-h-screen" : ""} `}>
-      </div>
-    </div>
-  );
+export default function Home() {
+const [menuActive, setMenuActive] = useState(false);
+    return (
+        <div className={` mx-auto w-full max-w-[1488px] flex-1 lg:grid lg:grid-cols-[280px_minmax(0,1fr)] lg:gap-14`}>
+            <Menu active={menuActive} setMenuActive={setMenuActive} />
+            <div className={`${menuActive ? "overflow-hidden max-h-screen" : ""} `}>
+                <MainArea slug="/" setMenuActive={setMenuActive} />
+            </div>
+        </div>
+    );
 }
